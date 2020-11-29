a42fff97b73189a8685eb263ff4f954939b0249d
ghimiredipesh <ghimiredipesh2020@gmail.com>
2020-11-29 16:32:37 +0545

    checkout page added

---
+++ lib/pages/check_out_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Checkout extends StatefulWidget {
+  @override
+  _CheckoutState createState() => _CheckoutState();
+}
+
+class _CheckoutState extends State<Checkout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
